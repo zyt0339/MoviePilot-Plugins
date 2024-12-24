5c273dd9631466303a379de2abbb4b7bd7db36f4
zyt0339 <zyt0339@gmail.com>
2024-12-25 00:40:36 +0800

    Update __init__.py

--- plugins.v2/iyuuautoseedzyt/__init__.py
+++ plugins.v2/iyuuautoseedzyt/__init__.py
@@ -33,7 +33,7 @@ class IYUUAutoSeedzyt(_PluginBase):
     # 插件图标
     plugin_icon = "Iyuu_A.png"
     # 插件版本
-    plugin_version = "2.2.0.1"
+    plugin_version = "2.2.0.2"
     # 插件作者
     plugin_author = "zyt"
     # 作者主页
@@ -41,7 +41,7 @@ class IYUUAutoSeedzyt(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "iyuuautoseedzyt_"
     # 加载顺序
-    plugin_order = 17
+    plugin_order = 4
     # 可使用的用户级别
     auth_level = 2
 
