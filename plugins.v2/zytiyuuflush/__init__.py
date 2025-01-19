18c9eab3ec5a244b5b0df0a5f6cb9a86f4aad754
zyt0339 <zyt0339@gmail.com>
2025-01-19 12:58:30 +0800

    辅种

--- plugins.v2/zytiyuuflush/__init__.py
+++ plugins.v2/zytiyuuflush/__init__.py
@@ -1157,7 +1157,7 @@ class ZYTIYUUflush(_PluginBase):
         判断种子是否可以做种并处于暂停状态
         """
         try:
-            return torrent.get("state") == "pausedUP" if dl_type == "qbittorrent" \
+            return torrent.get("state") in ["pausedUP", "stoppedUP"] if dl_type == "qbittorrent" \
                 else (torrent.status.stopped and torrent.percent_done == 1)
         except Exception as e:
             print(str(e))
